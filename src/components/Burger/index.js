70f68aa33ed7b714578d1ea60c757be59ed83b57
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-09-05 15:36:01 +0530

    null ingredients notice done!

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -3,11 +3,19 @@ import "./styles.css";
 import BurgerIngredients from "./BurgerIngredients";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredients key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredients key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className="Burger">
