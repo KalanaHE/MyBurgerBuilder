05e25732310c424b1e2c207f556254ea2e00dbf5
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-09-06 01:14:55 +0530

    Updates

---
+++ precache-manifest.e8e1b2c49742f26e429018f6caabd401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e30448b39811d690fcf95ba098018c",
+    "url": "/MyBurgerBuilder/index.html"
+  },
+  {
+    "revision": "0a1aacfd26048c066c70",
+    "url": "/MyBurgerBuilder/static/css/main.dcf08f1c.chunk.css"
+  },
+  {
+    "revision": "5868fa839e4e9844e37f",
+    "url": "/MyBurgerBuilder/static/js/2.d0e4fed9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MyBurgerBuilder/static/js/2.d0e4fed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1aacfd26048c066c70",
+    "url": "/MyBurgerBuilder/static/js/main.94e7ae19.chunk.js"
+  },
+  {
+    "revision": "84c50d490eaf64d89327",
+    "url": "/MyBurgerBuilder/static/js/runtime-main.fffe8286.js"
+  }
+]);
